d19490c8142e286015d7a9c611cc592e485e5339
Ryan Kruger <ryan.kruger91@gmail.com>
2020-12-26 12:54:57 -0600

    Fixed file structure to allow for testing on other device

---
+++ draw.py
@@ -0,0 +1,20 @@
+from PIL import ImageDraw, Image, ImageFont
+
+
+def image_draw(epd_width, epd_height):
+    font24 = ImageFont.truetype('Font.ttc', 24)
+    Himage = Image.new('1', (epd_width, epd_height), 255)
+    draw = ImageDraw.Draw(Himage)
+    draw.text((10, 0), 'hello world', font=font24, fill=0)
+    draw.text((10, 20), '7.5inch e-Paper', font=font24, fill=0)
+    draw.text((150, 0), u'微雪电子', font=font24, fill=0)
+    draw.line((20, 50, 70, 100), fill=0)
+    draw.line((70, 50, 20, 100), fill=0)
+    draw.rectangle((20, 50, 70, 100), outline=0)
+    draw.line((epd_width / 2, 0, epd_width / 2, epd_height), fill=0, width=2)
+    draw.line((165, 50, 165, 100), fill=0)
+    draw.line((140, 75, 190, 75), fill=0)
+    draw.arc((140, 50, 190, 100), 0, 360, fill=0)
+    draw.rectangle((80, 50, 130, 100), fill=0)
+    draw.chord((200, 50, 250, 100), 0, 360, fill=0)
+    return Himage
--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-import main
+from draw import image_draw
 
+
+def main():
+    img = image_draw(800, 480)
+    img.show()
+    return 0
+
+
+if __name__ == "__main__":
+    main()
