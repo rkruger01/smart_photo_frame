455fe255b8b0684525c870d2cc1f29c741b2bcee
Ryan Kruger <ryan.kruger91@gmail.com>
2020-12-26 16:01:33 -0600

    Updated weather parsing, added today's forecast

--- draw.py
+++ draw.py
@@ -1,5 +1,6 @@
 from PIL import ImageDraw, Image, ImageFont
 import calendar, datetime, requests
+
 weather_font_dict = {
     '01d': '\uf00d', '02d': '\uf002', '03d': '\uf013',
     '04d': '\uf012', '09d': '\uf01a ', '10d': '\uf019',
@@ -9,10 +10,13 @@ weather_font_dict = {
     '11n': '\uf03b', '13n': '\uf038', '50n': '\uf023'
 }
 
+
 def image_draw(epd_width, epd_height):
     textfont32 = ImageFont.truetype('CourierStd.otf', 32)
     textfont24 = ImageFont.truetype('CourierStd.otf', 24)
-    weatherfont = ImageFont.truetype('weathericons-regular-webfont.ttf', 48)
+    textfont16 = ImageFont.truetype('CourierStd.otf', 16)
+    weatherfont48 = ImageFont.truetype('weathericons-regular-webfont.ttf', 48)
+    weatherfont32 = ImageFont.truetype('weathericons-regular-webfont.ttf', 32)
     Himage = Image.new('1', (epd_width, epd_height), 255)
     draw = ImageDraw.Draw(Himage)
     # Generate divider lines
@@ -28,14 +32,43 @@ def image_draw(epd_width, epd_height):
     weather_toplefty = epd_height / 2
     draw.text((cal_topleftx + 10, cal_toplefty + 20), monthCal, font=textfont32, fill=0)
     # pull from weather service
-    draw.text((weather_topleftx + 5, weather_toplefty + 5), "Weather", font=textfont32)
-    colx = weather_topleftx + 30
-    coly = weather_toplefty + 50
-    draw.text((colx, coly), "Now", font=textfont32)
-    r = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=37.9514&lon=-91.7713&appid=d2a905d801e8a1b23ac0bcb292ec0949&units=imperial")
+    r = requests.get(
+        "https://api.openweathermap.org/data/2.5/onecall?lat=37.9514&lon=-91.7713&appid=d2a905d801e8a1b23ac0bcb292ec0949&units=imperial")
+    print("request complete")
     weather = r.json()
-    #Current Weather
-    draw.text((colx + 5, coly + 30), weather_font_dict[weather["current"]["weather"][0]["icon"]], font=weatherfont, fill=0)
-    draw.text((colx, coly + 100), str(weather["current"]["feels_like"]) + "°", font=textfont24, fill=0)
-    print(weather["current"]["feels_like"], "f degrees")
+    draw.text((weather_topleftx + 5, weather_toplefty + 5), "Weather", font=textfont32)
+    currentx = weather_topleftx + 30
+    forecasty = weather_toplefty + 50
+
+    # Current Weather
+    draw.text((currentx, forecasty), "Now", font=textfont32)
+    draw.text((currentx + 3, forecasty + 25), weather_font_dict[weather["current"]["weather"][0]["icon"]], font=weatherfont48,
+              fill=0)
+    draw.text((currentx + 10, forecasty + 100), str(round(weather["current"]["temp"])) + "°", font=textfont24, fill=0)
+    draw.text((currentx - 18, forecasty + 128), "Feels Like:", font=textfont16,
+              fill=0)
+    draw.text((currentx + 10, forecasty + 150), str(round(weather["current"]["feels_like"])) + "°", font=textfont24, fill=0)
+
+    # Today's Forecast
+    todayx = currentx + 110
+    draw.text((todayx, forecasty), "Today", font=textfont32)
+    draw.text((todayx + 15, forecasty + 25), weather_font_dict[weather["daily"][0]["weather"][0]["icon"]],
+              font=weatherfont48,
+              fill=0)
+    todaymintemp = round(weather["daily"][0]["temp"]["min"])
+    todaymaxtemp = round(weather["daily"][0]["temp"]["max"])
+    draw.text((todayx + 5, forecasty + 100), str(todaymaxtemp) + "°/" + str(todaymintemp) + "°", font=textfont24, fill=0)
+    precipxoffset = 10
+    precipyoffset = 120
+    if todaymintemp < 32:
+        # snow image!
+        draw.text((todayx + precipxoffset, forecasty + precipyoffset), weather_font_dict['13d'],
+                  font=weatherfont32,
+                  fill=0)
+    else:
+        # lame, no snow
+        draw.text((todayx + precipxoffset, forecasty + precipyoffset), weather_font_dict['10d'],
+                  font=weatherfont32,
+                  fill=0)
+    draw.text((todayx+precipxoffset+40, forecasty+precipyoffset+13),str(round(weather["daily"][0]["pop"])) + "%", font=textfont32, fill=0)
     return Himage
