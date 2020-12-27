b77057e358e78b9017d25f96dd43cee170b0b979
Ryan Kruger <ryan.kruger91@gmail.com>
2020-12-26 21:54:36 -0600

    Added gitignore. Updated draw.py to pull from config file. Weather fetcher now relies on zip + country. Weather now prints the location & time updated. Removed copyrighted font file.

--- draw.py
+++ draw.py
@@ -4,8 +4,9 @@ import logging
 import requests
 from math import floor
 from PIL import ImageDraw, Image, ImageFont
+import configparser
 
-stopPollWeatherAPI = True
+fetchWeather = True
 
 weather_font_dict = {
     '01d': '\uf00d', '02d': '\uf002', '03d': '\uf013',
@@ -17,10 +18,22 @@ weather_font_dict = {
 }
 
 
-def image_draw(epd_width, epd_height):
+def image_draw(epd_width, epd_height, config_file_name="config.txt"):
+    config = configparser.ConfigParser()
+    try:
+        config.read(config_file_name)
+        owm_api_key = config['Config']['owm_api_key']
+        positionstack_api_key = config['Config']['positionstack_api_key']
+        weather_units = config['Config']['units']
+        weather_zip = config['Config']['city_zip_code']
+        weather_country = config['Config']['city_country_code']
+    except configparser.MissingSectionHeaderError as e:
+        logging.error(e)
+        exit(1)
     textfont32 = ImageFont.truetype('CourierStd.otf', 32)
     textfont24 = ImageFont.truetype('CourierStd.otf', 24)
     textfont16 = ImageFont.truetype('CourierStd.otf', 16)
+    textfont12 = ImageFont.truetype('CourierStd.otf', 14)
     weatherfont48 = ImageFont.truetype('weathericons-regular-webfont.ttf', 48)
     weatherfont32 = ImageFont.truetype('weathericons-regular-webfont.ttf', 32)
     Himage = Image.new('1', (epd_width, epd_height), 255)
@@ -49,7 +62,8 @@ def image_draw(epd_width, epd_height):
         row = [' ' + (str(x)) if x < 10 else str(x) for x in row]  # list comprehension magic
         row = ['  ' if x == ' 0' else x for x in row]  # Remove zeros from calendar
         line = ' '.join(row)
-        logging.debug("Printing calendar row starting at {},{}".format(cal_topleftx+cal_x_offset, cal_toplefty + cal_y_offset + y))
+        logging.debug("Printing calendar row starting at {},{}".format(cal_topleftx + cal_x_offset,
+                                                                       cal_toplefty + cal_y_offset + y))
         draw.text((cal_topleftx + cal_x_offset, cal_toplefty + cal_y_offset + y), line, font=textfont32)
         y += 30
     offset_from_zero = now.day + leading_zeroes - 1
@@ -61,81 +75,95 @@ def image_draw(epd_width, epd_height):
     square_w, square_h = draw.textsize("00", font=textfont32)
     start_x = cal_topleftx + cal_x_offset + (true_date_w * today_x) - px_buffer
     start_y = cal_toplefty + cal_y_offset + (today_y * 30) + 28
-    logging.debug("Square starting at {},{}".format(start_x,start_y))
+    logging.debug("Square starting at {},{}".format(start_x, start_y))
     draw.rectangle([(start_x, start_y), (start_x + square_w + 2 * px_buffer, start_y + square_h + 2 * px_buffer)],
                    width=2)
 
-    if stopPollWeatherAPI:
-        return Himage
-    # pull from weather service
-    r = requests.get(
-        "https://api.openweathermap.org/data/2.5/onecall?lat=37.9514&lon=-91.7713&appid=d2a905d801e8a1b23ac0bcb292ec0949&units=imperial")
-    logging.debug("Openweathermap returned status code " + str(r.status_code))
-    weather = r.json()
+    if fetchWeather:
+        # geomap address to coordinates
+        query = weather_zip + "," + weather_country
+        r = requests.get("http://api.positionstack.com/v1/forward?access_key={}&query={}&limit=1".format(positionstack_api_key, query))
+        geolocation = r.json()
+        logging.debug("Weather fetched for {}".format(geolocation["data"][0]["label"]))
+        weather_lat = geolocation["data"][0]["latitude"]
+        weather_long = geolocation["data"][0]["longitude"]
+        # pull from weather service
+        r = requests.get(
+            "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&appid={}&units={}".format(weather_lat, weather_long, owm_api_key, weather_units))
+        logging.debug("Openweathermap returned status code " + str(r.status_code))
+        weather = r.json()
 
-    # Current Weather
-    weather_topleftx = epd_width / 2
-    weather_toplefty = epd_height / 2
-    draw.text((weather_topleftx + 5, weather_toplefty + 5), "Weather", font=textfont32)
-    currentx = weather_topleftx + 30
-    forecasty = weather_toplefty + 50
-    draw.text((currentx, forecasty), "Now", font=textfont32)
-    draw.text((currentx + 3, forecasty + 25), weather_font_dict[weather["current"]["weather"][0]["icon"]],
-              font=weatherfont48,
-              fill=0)
-    draw.text((currentx + 10, forecasty + 100), str(round(weather["current"]["temp"])) + "°", font=textfont24, fill=0)
-    draw.text((currentx - 18, forecasty + 128), "Feels Like:", font=textfont16,
-              fill=0)
-    draw.text((currentx + 10, forecasty + 150), str(round(weather["current"]["feels_like"])) + "°", font=textfont24,
-              fill=0)
+        # Current Weather
+        weather_header = "Weather"
+        weather_topleftx = epd_width / 2
+        weather_toplefty = epd_height / 2
+        w, h = draw.textsize(weather_header, font=textfont32)
+        draw.text((weather_topleftx + (cal_topleftx - w) / 2, weather_toplefty + 5), "Weather", font=textfont32)
+        currentx = weather_topleftx + 30
+        forecasty = weather_toplefty + 50
+        draw.text((currentx, forecasty), "Now", font=textfont32)
+        draw.text((currentx + 3, forecasty + 25), weather_font_dict[weather["current"]["weather"][0]["icon"]],
+                  font=weatherfont48,
+                  fill=0)
+        draw.text((currentx + 10, forecasty + 100), str(round(weather["current"]["temp"])) + "°", font=textfont24, fill=0)
+        draw.text((currentx - 18, forecasty + 128), "Feels Like:", font=textfont16,
+                  fill=0)
+        draw.text((currentx + 10, forecasty + 150), str(round(weather["current"]["feels_like"])) + "°", font=textfont24,
+                  fill=0)
 
-    # Today's Forecast
-    todayx = currentx + 100
-    draw.text((todayx, forecasty), "Today", font=textfont32)
-    draw.text((todayx + 15, forecasty + 25), weather_font_dict[weather["daily"][0]["weather"][0]["icon"]],
-              font=weatherfont48,
-              fill=0)
-    todaymintemp = round(weather["daily"][0]["temp"]["min"])
-    todaymaxtemp = round(weather["daily"][0]["temp"]["max"])
-    draw.text((todayx + 5, forecasty + 100), str(todaymaxtemp) + "°/" + str(todaymintemp) + "°", font=textfont24,
-              fill=0)
-    precipxoffset = 10
-    precipyoffset = 120
-    if todaymintemp < 32:
-        # snow image!
-        draw.text((todayx + precipxoffset, forecasty + precipyoffset), weather_font_dict['13d'],
-                  font=weatherfont32,
+        # Today's Forecast
+        todayx = currentx + 100
+        draw.text((todayx, forecasty), "Today", font=textfont32)
+        draw.text((todayx + 15, forecasty + 25), weather_font_dict[weather["daily"][0]["weather"][0]["icon"]],
+                  font=weatherfont48,
                   fill=0)
-    else:
-        # lame, no snow
-        draw.text((todayx + precipxoffset, forecasty + precipyoffset), weather_font_dict['10d'],
-                  font=weatherfont32,
+        todaymintemp = round(weather["daily"][0]["temp"]["min"])
+        todaymaxtemp = round(weather["daily"][0]["temp"]["max"])
+        draw.text((todayx + 5, forecasty + 100), str(todaymaxtemp) + "°/" + str(todaymintemp) + "°", font=textfont24,
                   fill=0)
-    draw.text((todayx + precipxoffset + 40, forecasty + precipyoffset + 13),
-              str(round(weather["daily"][0]["pop"])) + "%", font=textfont32, fill=0)
+        precipxoffset = 10
+        precipyoffset = 120
+        # today's precip chance
+        if todaymintemp < 32:
+            # snow image!
+            draw.text((todayx + precipxoffset, forecasty + precipyoffset), weather_font_dict['13d'],
+                      font=weatherfont32,
+                      fill=0)
+        else:
+            # lame, no snow
+            draw.text((todayx + precipxoffset, forecasty + precipyoffset), weather_font_dict['10d'],
+                      font=weatherfont32,
+                      fill=0)
+        draw.text((todayx + precipxoffset + 40, forecasty + precipyoffset + 13),
+                  str(round(weather["daily"][0]["pop"])) + "%", font=textfont32, fill=0)
 
-    # Tomorrow's Forecast
-    tomorrowx = todayx + 110
-    draw.text((tomorrowx, forecasty), "Tomorrow", font=textfont32)
-    draw.text((tomorrowx + 45, forecasty + 25), weather_font_dict[weather["daily"][1]["weather"][0]["icon"]],
-              font=weatherfont48,
-              fill=0)
-    tomorrowmaxtemp = round(weather["daily"][1]["temp"]["max"])
-    tomorrowmintemp = round(weather["daily"][1]["temp"]["min"])
-    draw.text((tomorrowx + 30, forecasty + 100), str(tomorrowmaxtemp) + "°/" + str(tomorrowmintemp) + "°",
-              font=textfont24,
-              fill=0)
-    precipxoffset += 30  # to fix scaling
-    if tomorrowmintemp < 32:
-        # snow image!
-        draw.text((tomorrowx + precipxoffset, forecasty + precipyoffset), weather_font_dict['13d'],
-                  font=weatherfont32,
+        # Tomorrow's Forecast
+        tomorrowx = todayx + 110
+        draw.text((tomorrowx, forecasty), "Tomorrow", font=textfont32)
+        draw.text((tomorrowx + 45, forecasty + 25), weather_font_dict[weather["daily"][1]["weather"][0]["icon"]],
+                  font=weatherfont48,
                   fill=0)
-    else:
-        # lame, no snow
-        draw.text((tomorrowx + precipxoffset, forecasty + precipyoffset), weather_font_dict['10d'],
-                  font=weatherfont32,
+        tomorrowmaxtemp = round(weather["daily"][1]["temp"]["max"])
+        tomorrowmintemp = round(weather["daily"][1]["temp"]["min"])
+        draw.text((tomorrowx + 30, forecasty + 100), str(tomorrowmaxtemp) + "°/" + str(tomorrowmintemp) + "°",
+                  font=textfont24,
                   fill=0)
-    draw.text((tomorrowx + precipxoffset + 40, forecasty + precipyoffset + 13),
-              str(round(weather["daily"][1]["pop"])) + "%", font=textfont32, fill=0)
+        precipxoffset += 30  # to fix scaling
+        # tomorrow's precip chance
+        if tomorrowmintemp < 32:
+            # snow image!
+            draw.text((tomorrowx + precipxoffset, forecasty + precipyoffset), weather_font_dict['13d'],
+                      font=weatherfont32,
+                      fill=0)
+        else:
+            # lame, no snow
+            draw.text((tomorrowx + precipxoffset, forecasty + precipyoffset), weather_font_dict['10d'],
+                      font=weatherfont32,
+                      fill=0)
+        draw.text((tomorrowx + precipxoffset + 40, forecasty + precipyoffset + 13),
+                  str(round(weather["daily"][1]["pop"])) + "%", font=textfont32, fill=0)
+
+        # Print weather location, update time
+        draw.text((weather_topleftx + 8, epd_height - 15), "{}".format(geolocation["data"][0]["label"]), font=textfont12)
+        draw.text((epd_width - 110, epd_height - 15), "Updated {}:{}".format(now.hour, now.minute), font=textfont12)
     return Himage
